250edd16f659fbf5649c832dbe44bf2ac0eae47e
Gsingh225 <gsingh5864@bths.edu>
2023-05-09 12:14:58 +0000

    tab nav minor

--- Muzic/App.js
+++ Muzic/App.js
@@ -1,12 +1,27 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialBottomTabNavigator } from "react-navigation-material-bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+const TabNavigator = createMaterialBottomTabNavigator() {
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: "Home",
+      tabBarIcon: (tabinfo) => (
+        <Ionicons 
+          name="md-home"
+          size={tabinfo.focused ? 26:20}
+          color={tabinfo.tintColor}
+        />
+      )
+    }
+  }
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    
   );
 }
 
